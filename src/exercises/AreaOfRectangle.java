3e1c8926da83e56d86ff043a329a331415c386ce
Karina Lage <karinar_lage@live.com>
2022-08-02 10:41:11 -0500

    Finished exercises class 1

---
+++ src/exercises/AreaOfRectangle.java
@@ -0,0 +1,19 @@
+package exercises;
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the rectangle length?");
+        double length = input.nextDouble();
+
+        System.out.println("What is the rectangle width?");
+        double width = input.nextDouble();
+
+        double area = length*width;
+
+        System.out.println("The are of the rectangle is " + area);
+
+    }
+}
