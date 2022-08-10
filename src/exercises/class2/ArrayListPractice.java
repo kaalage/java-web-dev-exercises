cfb7583140adc7bd50c5ff9b7e6acbcfa7901a45
Karina Lage <karinar_lage@live.com>
2022-08-10 18:42:25 -0500

    Finished exercises class 2

--- src/exercises/class2/ArrayListPractice.java
+++ src/exercises/class2/ArrayListPractice.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayListPractice {
-    public static int sumEven(ArrayList<Integer> arr) {
-        int total = 0;
+    public static void main(String [] args) {
+        ArrayList<Integer> numList = new ArrayList<>();
+        numList.add(1);
+        numList.add(2);
+        numList.add(3);
+        numList.add(4);
+        numList.add(5);
+        numList.add(6);
+        numList.add(7);
+        numList.add(8);
+        numList.add(9);
+        numList.add(10);
+        int answer = sumEven(numList);
+        System.out.println(answer);
+    }
 
-        for (int integer : arr) {
-            if (integer % 2 == 0) {
-                total += integer;
+    public static int sumEven(ArrayList<Integer> numList) {
+        int total = 0;
+        for (int num: numList) {
+            if (num % 2 == 0) {
+                total += num;
             }
         }
         return total;
-
     }
 
-    public static void main(String [] args) {
-        int[] arrayList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println();
-    }
+
 }
