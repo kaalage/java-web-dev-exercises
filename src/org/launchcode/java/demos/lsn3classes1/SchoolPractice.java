3b165fdee5db1b2801853eb582b497dcbaeb9e6f
Karina Lage <karinar_lage@live.com>
2022-08-14 17:19:56 -0500

    Finished exercises class 3

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,20 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class SchoolPractice {
     public static void main(String[] args) {
-        // Instantiate your Student class for part 2 here!
+        public class Course {
+            private String topic;
+            private Teacher instructor;
+            private ArrayList<Student> enrolledStudents;
+        }
+
+        public class Teacher {
+            private String firstName;
+            private String lastName;
+            private String subject;
+            private int yearsTeaching;
+        }
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,9 +5,44 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
+    String name = "Karina";
+    Integer numberOfCredits = 1;
+    Double gpa = 4.0;
+
     private String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     private int studentId;
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
     private int numberOfCredits = 0;
+    public void setnumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
     private double gpa = 0.0;
+    public void setgpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
 
 }
